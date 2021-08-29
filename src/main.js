dee3363603d76dfb7dd1121e3bea64234c61aff3
liuxz <texas7lee@foxmail.com>
2021-08-29 22:07:56 +0800

    项目初始化结束，开始登录注册模块

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from './store'
 
 import './styles/index.less'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 加载动态设置REM基准值
+import 'amfe-flexible'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
