494f8578da16ac1cd6d67400cccb45131bc06b85
liuxz <texas7lee@foxmail.com>
2021-08-30 14:41:39 +0800

    提交测试，因为别的仓库提交不上去

---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login-container">dengluyeian</div>
+</template>
+
+<script>
+export default {
+  name: 'loginIndex',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
